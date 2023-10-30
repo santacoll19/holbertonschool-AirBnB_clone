59fa34b860f282f118a96f7e980f8c2f461ec08b
Joshua7792 <j.santiago39@hotmail.com>
2023-10-30 15:29:18 -0400

    #4 Update models/base_model.py:

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,18 @@ from datetime import datetime
 class BaseModel:
     """This is the BaseModel class"""
 
-    def __init__(self):
-        """Initialization of the class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            """Initialization of the class"""
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns the string representation of the class"""
