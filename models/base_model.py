1135ddf66d7cb71fbc2c1d64d1afbfd9bc3b6755
Joshua7792 <j.santiago39@hotmail.com>
2023-10-30 15:19:41 -0400

    fix for checkers

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
 """Unittest for BaseModel class"""
+
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """BaseModel class"""
+    """This is the BaseModel class"""
+
     def __init__(self):
-        """Initialize variables"""
+        """Initialization of the class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """ Print a str"""
-        return f"[{self.__class__.__name__}] ({self.id} {self.__dict__})"
+        """Returns the string representation of the class"""
+        return F"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
 
     def save(self):
-        """ Update the attribute updated_at"""
-        self.updated_at = datetime.now()
+        """Updates the public instance attribute updated_at"""
+        self.update_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__"""
+        """Returns a dictionary"""
         class_dict = dict(self.__dict__)
         class_dict["__class__"] = self.__class__.__name__
         class_dict["created_at"] = self.created_at.isoformat()
