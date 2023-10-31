3797fee8ec458c0baf8c03006f29281eb8e9f03a
Joshua7792 <j.santiago39@hotmail.com>
2023-10-31 13:11:29 -0400

    fixed base model

--- models/base_model.py
+++ models/base_model.py
@@ -10,29 +10,14 @@ class BaseModel:
     """BaseModels class"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize BaseModels instance"""
         if kwargs:
-            # For each key in the dictionary, set the attribute with \
-            # the associated value.
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    # Convert string datetime to datetime object.
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-                # Check if 'id' is not in kwargs, then assign it.
-            if "id" not in kwargs.keys():
-                self.id = str(uuid.uuid4())
-            # Check if 'created_at' is not in kwargs, then assign it.
-            if "created_at" not in kwargs.keys():
-                self.created_at = datetime.now()
-            # Check if 'updated_at' is not in kwargs, then assign it
-            if "updated_at" not in kwargs.keys():
-                self.updated_at = datetime.now()
-            if not kwargs:
-                models.storage.new(self)
         else:
-            """initialize variables"""
+            """Initialization of the class"""
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
