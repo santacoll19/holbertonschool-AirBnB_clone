9e7234fc6a606621a52e9315392415f12fc55858
Joshua7792 <j.santiago39@hotmail.com>
2023-11-01 15:12:13 -0400

    class user

--- models/user.py
+++ models/user.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 
-
 import models
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """This class defines a user"""
-    
     email = ""
     password = ""
     first_name = ""
