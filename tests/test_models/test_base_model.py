246cb53627e9c3b3362ed99cba6359fbf9f1f440
Joshua7792 <j.santiago39@hotmail.com>
2023-11-03 13:42:42 -0400

    replace the tests files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,40 +1,14 @@
-import unittest
+#!/usr/bin/python3
 from models.base_model import BaseModel
-from datetime import datetime
 
-class TestBaseModel(unittest.TestCase):
-    def test_base_model_attributes(self):
-        """Test BaseModel class attributes"""
-        base_model = BaseModel()
-        self.assertTrue(hasattr(base_model, 'id'))
-        self.assertIsInstance(base_model.id, str)
-        self.assertTrue(hasattr(base_model, 'created_at'))
-        self.assertIsInstance(base_model.created_at, datetime)
-        self.assertTrue(hasattr(base_model, 'updated_at'))
-        self.assertIsInstance(base_model.updated_at, datetime)
-
-    def test_base_model_str(self):
-        """Test the __str__ method"""
-        base_model = BaseModel()
-        expected_str = f"[{base_model.__class__.__name__}] ({base_model.id})"
-        self.assertIn(expected_str, str(base_model))
-
-    def test_base_model_save(self):
-        """Test the save method"""
-        base_model = BaseModel()
-        original_updated_at = base_model.updated_at
-        base_model.save()
-        new_updated_at = base_model.updated_at
-        self.assertGreater(new_updated_at, original_updated_at)
-
-    def test_base_model_to_dict(self):
-        """Test the to_dict method"""
-        base_model = BaseModel()
-        base_model_dict = base_model.to_dict()
-        self.assertIsInstance(base_model_dict, dict)
-        self.assertEqual(base_model_dict['__class__'], 'BaseModel')
-        self.assertIsInstance(base_model_dict['created_at'], str)
-        self.assertIsInstance(base_model_dict['updated_at'], str)
-
-if __name__ == '__main__':
-    unittest.main()
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
